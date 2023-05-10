9f6752c1c39b3ba1c17dc43055f6595932e05b65
bracq <florianbracq42@gmail.com>
2023-05-10 17:20:42 +0200

    d

--- todo/settings.py
+++ todo/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import environ
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -83,11 +86,11 @@ DATABASES = {
 
         'ENGINE': 'django.db.backends.mysql',
 
-        'NAME': 'MYSQL-DATABASE-NAME',                     
+        'NAME': env('DATABASE_NAME'),                     
 
-        'USER': 'MYSQL-SERVER-USER-NAME',                     
+        'USER': env('DATABASE_USER'),                     
 
-        'PASSWORD': 'MYSQL-SERVER-USER-PASSWORD',                  
+        'PASSWORD': env('DATABASE_PASSWORD'),                  
 
         'HOST': 'MySQL-SERVER-NAME',                     
 
