bf77a87a30c8de2bd47099ff5ad428ae5423a5f0
bracq <florianbracq42@gmail.com>
2023-05-11 11:21:38 +0200

    reee

--- todo/settings.py
+++ todo/settings.py
@@ -120,12 +120,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+SCM_DO_BUILD_DURING_DEPLOYMENT=True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-
+STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
